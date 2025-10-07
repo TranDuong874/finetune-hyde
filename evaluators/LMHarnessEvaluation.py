9fe2b06bc464c272306db74a72cf16f9fe2db286
Trần Thái Dương <124498786+TranDuong874@users.noreply.github.com>
2025-10-07 06:48:09 +0000

    fix

--- evaluators/LMHarnessEvaluation.py
+++ evaluators/LMHarnessEvaluation.py
@@ -5,33 +5,53 @@ import torch
 import numpy as np
 import copy
 
+
 class LMHarnessEvaluation:
     def __init__(self, model, tokenizer, harness_eval_config):
         try:
             self.model = model
             self.tokenizer = tokenizer
             self.harness_eval_config = harness_eval_config
+            
+            # Base line:
+            # https://huggingface.co/google/gemma-3-270m-it
+            # Reasoning and factuality evaluataion
+
+            self.task_list = [
+                "bbh",
+                "gpqa",
+                "ifeval"
+            ]
+
         except Exception as e:
             print(e)
 
 
     def eval(self):
-        self.model.to("cuda")
         language_model = HFLM(
             pretrained=self.model,
             tokenizer=self.tokenizer,
             device="cuda"
         )
-        
+
         print(language_model.device)
 
         eval_results = evaluator.simple_evaluate(
             model=language_model,
+            tasks=self.task_list,
             **self.harness_eval_config
-        )
+        )['results']
 
-        with open('harness_results.json', 'w', encoding='utf-8') as file:
-            json.dump(eval_results['results'], file, indent=4, ensure_ascii=False)
+        
+        selected_results = {
+            'bbh' : eval_results['bbh']['exact_match,get-answer'],
+            'gpqa' : eval_results['gpqa_diamond_zeroshot']['acc,none'],
+            # 'ifeval' : eval_results['ifeval']
+        }
+        
+        with open('harness_result_reference.json', 'w', encoding='utf-8') as file:
+            json.dump(eval_results, file, indent=4, ensure_ascii=False)
 
-        return json.dumps(eval_results['results'], indent=4, ensure_ascii=False)
+        print(selected_results)
+        return selected_results
 
